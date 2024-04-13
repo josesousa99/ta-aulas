56abf4326698d7af6c1db0cb1bf17da773d236cb
Jose Sousa <aluno20488@ipt.pt>
2024-04-13 20:12:35 +0100

    Criação do projeto

---
+++ ProjetoFinal20488/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoFinal20488.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
